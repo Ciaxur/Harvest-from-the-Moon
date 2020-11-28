46c1e238984115e02eeaba50e09655d7dee4e48f
david-bob-adrian <72585073+david-bob-adrian@users.noreply.github.com>
2020-11-28 14:23:52 -0500

    Added Music
    Added settings manager (for user to change music/sound effect volume); added music manager to play music; added test track (which might be updated later).

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : Singleton<SettingsManager>
+{
+    public bool musicMuted = false;
+    public float musicVolume = .7f;
+
+    public bool soundsMuted = false;
+    public float soundsVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
