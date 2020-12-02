cd1ce711e682f6928408f890a23ed5a260ca9f42
Omar Omar <oomar@umich.edu>
2020-12-02 17:00:19 -0500

    Completed Level Transition Animation & Level Prefabs
    - All Levels have Prefabs and Scenes with appropriate Components
    - Organized Assets to Categories

---
+++ Assets/Scripts/Level/SceneTransitions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneTransitions : MonoBehaviour {
+    // External References
+    public Animator fadeTransition;
+    
+
+    /**
+     * Handles Transitions between Scenes
+     * @param sceneName String Name of Scene
+     */
+    public void GoToScene(string sceneName) {
+        StartCoroutine(PlayGoToMenu(sceneName));
+    }
+
+    // Starts Animation to transition to Main Menu
+    public IEnumerator PlayGoToMenu(string sceneName) {
+        fadeTransition.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/Menu/SceneButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneButtonScript : MonoBehaviour
+{
+    Button button;
+    public string SceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => {
+            whenClicked();
+        });
+    }
+
+    void whenClicked()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
