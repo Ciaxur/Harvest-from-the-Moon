f22b4b99cb701b81f8a19153f9c4e103de03c19d
david-bob-adrian <72585073+david-bob-adrian@users.noreply.github.com>
2020-12-07 18:49:52 -0500

    Hogweed Pond
    Hogweed pond now damages enemies; it's also suppose to make them jump a little, but it doesn't seem to be working. I had tried some ridiculously large forces, so that's not it; maybe it has to do with the nav agent?

---
+++ Assets/Scripts/Player/HogweedPond.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HogweedPond : MonoBehaviour {
+    // External Settings
+    public int damage = 1;
+    public float impulseAmount = 400;
+
+    private Dictionary<GameObject, float> enemies;
+
+    void Start() {
+        enemies = new Dictionary<GameObject, float>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        // Collided with Enemy
+        if (other.tag == "Enemy") {
+            enemies.Add(other.gameObject, 0.0f);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.tag == "Enemy") {
+            float newValue = enemies[other.gameObject] + Time.deltaTime;
+            if (newValue > .2f && newValue * damage >= 1.0f)
+            {
+                other.GetComponent<Attributes>().inflictDamage((int) newValue);
+                newValue -= (int) newValue;
+                other.GetComponent<Rigidbody>().AddForce(new Vector3(0, impulseAmount, 0), ForceMode.Impulse);
+            }
+            enemies[other.gameObject] = newValue;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Enemy") {
+            int remainingDamage = (int) (enemies[other.gameObject] * damage);
+            if (remainingDamage >= 1)
+            {
+                other.GetComponent<Attributes>().inflictDamage(remainingDamage);
+            }
+            enemies.Remove(other.gameObject);
+        }
+    }
+}
